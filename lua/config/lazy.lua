a0187e4d026bdf6dd524f3442bdb267fd0854a23
Elias Rissanen Uran <elura001@osloskolen.no>
2023-11-12 13:59:18 +0100

    discord

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,37 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    "catppuccin/nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "fatih/vim-go",
-    "kylechui/nvim-surround",
-    "sbdchd/neoformat",
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {}
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+  "catppuccin/nvim",
+  "nvim-treesitter/nvim-treesitter",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+  "fatih/vim-go",
+  "kylechui/nvim-surround",
+  "sbdchd/neoformat",
+  "andweeb/presence.nvim",
+  { "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.4', dependencies = { 'nvim-lua/plenary.nvim' }}
 }
 
 local opts = {}
